aaca443372907da2efa07d04b0ce62006386e476
maetomo1021 <maetomoda@gmail.com>
2025-02-17 10:58:34 +0900

    スクレイピング練習

---
+++ flask_app/BeautifulSoup.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+url = "https://www.jcb.co.jp/voucher/gift-card/merchant.html"
+response = requests.get(url)
+
+# 文字化けを防ぐためにエンコーディングを明示的に指定
+response.encoding = response.apparent_encoding  # 自動検出されたエンコーディングを適用
+
+# BeautifulSoupで解析
+print("全部のDOM取得")
+soup = BeautifulSoup(response.text,"html.parser")
+# print(response.text)
+
+print("飲食店のチェーン店名を取得")
+# 全ての <td> 要素を取得
+td_elements = soup.find_all("td")
+print("確認用")
+print(td_elements)
+
+# テキストだけを取得して表示
+for i, td in enumerate(td_elements, 1):
+    if (148 <=i and i<= 172):
+        print(f"{i}: {td.text.strip()}")
+    
--- flask_app/bot.py
+++ flask_app/bot.py
@@ -1,5 +1,8 @@
 import discord
 import asyncio
+import requests
+import json
+from bs4 import BeautifulSoup
 
 TOKEN = "NzkwMTA0MTU4MzI1NDQwNTQz.GRnXyK.nl1CcCgnGZXos1d0oegaJf-AcC9bm-OjMBtibQ"  # Botのトークン
 CHANNEL_ID = 1337546857919938700  # メッセージを送るチャンネルのID
@@ -24,8 +27,37 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("こんにちは！")
 
+# async def ShareJson(ctx,channel:discord.TextChannel,*,message):
+#     await channel.send(message)
+#     # !send #general で、generalチャンネルにメッセージを送信する　らしい
 
 
+async def fetch_and_send():
+    url = "http://127.0.0.1:5000/"  # FlaskのURL
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # 必要なデータを取得
+        title = soup.find("li").text
+        content = soup.find("p", {"id": "content"}).text
+
+        # JSONデータに加工
+        data = {
+            "title": title,
+            "content": content,
+            "url":url
+        }
+        json_data = json.dumps(data, indent=4, ensure_ascii=False)
+
+        # Discordに送信
+        channel = client.get_channel(CHANNEL_ID)
+        if channel:
+            await channel.send(f"📢 データを取得しました！\n```json\n{json_data}\n```")
+        else:
+            print("チャンネルが見つかりません")
+    else:
+        print("Webページの取得に失敗しました")
 
 if __name__ == "__main__":
     asyncio.run(client.start(TOKEN))
